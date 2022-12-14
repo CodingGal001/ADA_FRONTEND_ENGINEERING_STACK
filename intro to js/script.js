a02fab652e7c9bbe1af3d1e779fa0a6e3130e5cb
codinggal001 <adeyemoayomide9899@gmail.com>
2022-12-14 14:12:08 -0800

    javascript

---
+++ intro to js/script.js
@@ -0,0 +1,7 @@
+let username;
+let age;
+
+username= prompt("Enter your username", "Ayomide Adeyemo"); 
+age= prompt("Input your age");
+
+alert( "Welcome, " +username+ "," + "You are "+age+ " years old." )
